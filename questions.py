67cf8fd0b827a77e60f0c382b38b64a0c304d221
lasyasreepada <lasya.sreepada@yale.edu>
2017-05-07 19:24:12 -0400

    Added questions to the quiz

--- questions.py
+++ questions.py
@@ -12,18 +12,35 @@ import time
 def quiz():
 
     questions = [
-        ("cp1", "1", "cp"),
-        ("cp2", "2", "cp"),
-        ("cp3", "3", "cp"),
-        ("cp4", "4", "cp"),
-        ("tm1", "1", "tm"),
-        ("tm2", "2", "tm"),
-        ("tm3", "3", "tm"),
-        ("tm4", "4", "tm"),
-        ("pt1", "1", "pt"),
-        ("pt2", "2", "pt"),
-        ("pt3", "3", "pt"),
-        ("pt4", "4", "pt")
+        ("Copyright protects both expression of an idea and the idea itself. \nTrue or False?", "f", "cp"),
+
+        ("Clothing, such as Katy Perry’s “Left Shark” costume is a useful article and is therefore copyrightable. \nTrue or False?", "f", "cp"),
+
+        ("One of the factors of evaluation for fair use is the effect of the use upon the potential market for the work in question. \nTrue or False?", "t", "cp"),
+
+        ("In Cariou vs. Prince, the defendant was brought to court because he used images from Cariou’s 2000 book, Yes Rasta, to create a new exhibition of photos with some apparent modifications. This was not fair use because it did not comment on the original work about the nature of the photographs. \nTrue or False?", "t", "cp"),
+
+        ("Copyright is an inevitable, “divine” grant entrusting total ownership rights to the creator of a work. \nTrue or False?", "f", "cp"),
+
+        ("In Christian Louboutin vs. YSL, the defendant was brought to court for using a red outsole on women’s shoes that were also red in color. The court ruled that this was a trademark infringement. \nTrue or False? ", "f", "tm"),
+
+        ("Descriptive names for a company or product (e.g. FishFri) are never trademarkable. \nTrue or False?", "f", "tm"),
+
+        ("Companies such as Google can potentially lose their trademark protection because of genericide. \nTrue or False?", "t", "tm"),
+
+        ("The same trademark (e.g. a word) can be registered by different parties, so long as the trademarks are in different classes. \nTrue or False?", "t", "tm"),
+
+        ("Trademarked goods or services must be made available for commercial sale on a national level (beyond state boundaries). \nTrue or False?", "t", "tm"),
+
+        ("A utility patent application consists of three parts: drawings, a written description, and claim statements. The drawings of the product are most important in determining what exactly gets patented. \nTrue or False?", "f", "pt"),
+
+        ("Naturally occurring processes or products, such as human DNA, are not patentable. \nTrue or False?", "t", "pt"),
+
+        ("The tests that the court used in Alice Corp v. CLS Bank Intl to determine whether the computer software was patent eligible were (1) whether the claims directed to an abstract idea and (2) whether the claims added something inventive. \nTrue or False?", "t", "pt"),
+
+        ("The level of skill required to develop a product or process is not considered when determining whether it satisfies the non-obviousness requirement for patent eligibility. \nTrue or False?", "f", "pt"),
+
+        ("There is a distinction between utility and design, therefore, it is possible for one product to have both a utility patent and a design patent. \nTrue or False?", "t", "pt")
     ]
 
     shuffle(questions)
@@ -35,7 +52,7 @@ def quiz():
     time.sleep(2)
     print("We will ask you a series of true or false questions about various cases related with copyrights, trademarks, and patents.")
     time.sleep(5)
-    print("When a question is displayed, you will be prompted for an answer. Please type a for true, and b for false.")
+    print("When a question is displayed, you will be prompted for an answer. Please type t for true, and f for false.")
     time.sleep(5)
     print("At the end of the quiz, we will sort you into a branch of IP Law based on your quiz performance. Good Luck!")
     time.sleep(5)
@@ -62,9 +79,9 @@ def quiz():
     if total_correct == len(questions):
         print("Congratulations, you are the IP Law Supreme Overlord! You got all the questions right and would do well in any branch.")
     else:
-        if (cp_correct > tm_correct and cp_correct > pt_correct):
+        if (cp_correct >= tm_correct and cp_correct >= pt_correct):
             print("You are Copyright Law!")
-        if (tm_correct >= cp_correct and tm_correct >= pt_correct):
+        elif (tm_correct >= cp_correct and tm_correct >= pt_correct):
             print("You are Trademark Law!")
         else:
             print("You are Patent Law!")
