0bfaedf9563a625405ecac8871bbd63bbcf94a36
lasyasreepada <lasya.sreepada@yale.edu>
2017-05-07 16:04:17 -0400

    renamed app.py

---
+++ quizgui.py
@@ -0,0 +1,18 @@
+"""This file provides the GUI for the IP Law quiz
+
+quizqui.py
+Lasya Sreepada
+Yale College '19
+
+May 7, 2017
+"""
+
+from tkinter import *
+
+#create the window
+root = tk()
+
+#modify root window
+root.title("Simple 
+
+
